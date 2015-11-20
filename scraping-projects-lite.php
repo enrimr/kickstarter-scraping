e59e4de76aebc9eacbb97b3af24c80df91c28afb
Enrique Mendoza <enrique.mendoza@hightrack.me>
2015-11-20 18:20:00 +0100

    Add scraping-projects-lite

---
+++ scraping-projects-lite.php
@@ -0,0 +1,29 @@
+<?php
+include_once("simple_html_dom.php");
+
+$prefix = 'https://www.kickstarter.com';
+
+for ($userId=60000; $userId < 3000000000; $userId++) { 
+	// Create DOM from URL
+	$continue = true;
+	$page = 1;
+	do {
+		$html = file_get_html("$prefix/profile/$userId?page=$page");
+		if (strcmp($html, "") !== 0) {
+
+			/*foreach ($html->find('.project_item') as $project) {
+				//echo "p ";
+				//echo "\n >> $project->href";
+				$projects[$project->href][] = $userId;
+			}*/
+			if ($html->find('.no-content')){
+				$continue = false;
+			} else {
+				echo "\nUserId: $userId\n";
+				$continue = false;
+			}
+		} else {
+			$continue = false;
+		}
+	} while ( $continue );
+}
